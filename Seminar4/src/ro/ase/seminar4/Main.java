ee60b12e39ca6f83b4a95527751482983ac9c100
LaviniaD135 <63166074+LaviniaD135@users.noreply.github.com>
2021-03-18 09:38:42 +0200

    seminar 4

---
+++ Seminar4/src/ro/ase/seminar4/Main.java
@@ -0,0 +1,10 @@
+package ro.ase.seminar4;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ShoppingCart shoppingCart=ShoppingCart.getInstance();
+		 shoppingCart=ShoppingCart.getInstance();
+		
+	}
+}
